a559aaee2413bbaf63baca5b31fcc4b13a34599e
naidu-kotha <srinivasnaidu1605@gmail.com>
2022-10-08 11:18:26 +0000

    adds Get All Players API

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+const path = require("path");
+const dbPath = path.join(__dirname, "cricketMatchDetails.db");
+
+let db = null;
+
+
+// Initialization
+const initializeDbAndServer = async() => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        });
+        app.listen(3000, () => {
+            console.log("Server Running at http://localhost:3000");
+        });
+    } catch(e) {
+        console.log(`DBError: ${e.message}`);
+    };
+};
+
+initializeDbAndServer();
+
+// Get All Players API
+app.get("/players/", async(request, response) => {
+    const getPlayersQuery = `
+    SELECT
+      player_id as playerId,
+      player_name as playerName
+    FROM
+      player_details;`;
+
+    const players = await db.all(getPlayersQuery);
+
+    response.send(players);
+});
+
+
+
+module.exports = app;
